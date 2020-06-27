2b5cee44e4b379d42528a14db6a5ecf12c26b25f
bistudio <basswrks@hotmail.com>
2020-06-27 13:48:30 +0100

    implement summary.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f257b913bf5f06fc9680179dbc27f867'
@@ -7,5 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+s = Serializer(app.config['SECRET_KEY'], 1800)
 
 from app import routes
